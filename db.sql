8273e240c7e6f6b215e6c959f4c90d4beda3885c
golimagi <42090424+golimagi@users.noreply.github.com>
2018-08-04 19:53:22 +0800

    Bug fix mysql

--- db.sql
+++ db.sql
@@ -1,4 +1,12 @@
-CREATE TABLE `group` (
+DROP TABLE IF EXISTS `usergroup`;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `category`;
+DROP TABLE IF EXISTS `video`;
+DROP TABLE IF EXISTS `danmaku`;
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `usergroup` (
 	`id` INT AUTO_INCREMENT,
 	`name` VARCHAR(20) NOT NULL,
 	`description` TEXT NOT NULL,
@@ -14,25 +22,25 @@ CREATE TABLE `user` (
 	`blanklisted` TINYINT(1) NOT NULL DEFAULT 0,
 	`status` TINYINT(1) NOT NULL DEFAULT 1,
 	`reg_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`lastlogin_time` TIMESTAMP DEFAULT NULL,
+	`lastlogin_time` TIMESTAMP,
 	UNIQUE (`name`),
 	PRIMARY KEY (`id`),
-	FOREIGN KEY (`group`) REFERENCES group(`id`)
+	FOREIGN KEY (`group`) REFERENCES usergroup(`id`)
 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `category` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(20) NOT NULL,
-	`description` TEXT NOT NULL DEFAULT '',
+	`description` TEXT NOT NULL,
 	PRIMARY KEY (`id`)
 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `video` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`title` TEXT NOT NULL DEFAULT 'No Title',
+	`title` TEXT NOT NULL,
 	`upload_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`status` TINYINT(1) NOT NULL,
-	`description` TEXT NOT NULL DEFAULT '',
+	`description` TEXT NOT NULL,
 	`duration` TIME NOT NULL,
 	`owner` INT NOT NULL,
 	`play_times` INT NOT NULL DEFAULT 0,
@@ -51,7 +59,7 @@ CREATE TABLE `danmaku` (
 	`position` TINYINT(1) NOT NULL DEFAULT 0,
 	`offset` TIME NOT NULL,
 	`created_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`id`),
 	FOREIGN KEY (`video`) REFERENCES video(`id`),
 	FOREIGN KEY (`owner`) REFERENCES user(`id`)
 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
@@ -63,7 +71,7 @@ CREATE TABLE `comment` (
 	`content` TEXT NOT NULL,
 	`reply_to` INT DEFAULT NULL,
 	`created_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`modified_time` TIMESTAMP DEFAULT NULL,
+	`modified_time` TIMESTAMP,
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`video`) REFERENCES video(`id`),
 	FOREIGN KEY (`owner`) REFERENCES user(`id`)
@@ -76,7 +84,7 @@ CREATE TABLE `message` (
 	`title` TEXT NOT NULL,
 	`content` TEXT,
 	`sent_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`read_time` TIMESTAMP DEFAULT NULL,
+	`read_time` TIMESTAMP,
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`from`) REFERENCES user(`id`),
 	FOREIGN KEY (`to`) REFERENCES user(`id`)
